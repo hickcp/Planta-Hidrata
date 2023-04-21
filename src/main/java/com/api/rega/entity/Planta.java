932d60618e2e81524a9a1d0137f98f2e2b4f42d1
hickcp <henrique_c_p@outlook.com>
2023-04-21 20:36:56 -0300

    DTOs, Entity, Repository and Services added

--- src/main/java/com/api/rega/entity/Planta.java
+++ src/main/java/com/api/rega/entity/Planta.java
@@ -1,4 +1,26 @@
 package com.api.rega.entity;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "plantas")
+@Entity(name = "Planta")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Planta {
+
+    private Long id;
+    private String nome;
+    private String nomeCientifico;
+
+    @OneToMany(mappedBy = "id_planta")
+    private PlantaInformacoes informacoes;
 }
--- src/main/java/com/api/rega/entity/PlantaInformacoes.java
+++ src/main/java/com/api/rega/entity/PlantaInformacoes.java
@@ -1,4 +1,23 @@
 package com.api.rega.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "plantaInformacoes")
+@Entity(name = "PlantaInformacoes")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class PlantaInformacoes {
+    Long id;
+    Long id_planta;
+    float luzSolar;
+    float umidade;
+    float temperatura;
+    boolean regar;
 }
