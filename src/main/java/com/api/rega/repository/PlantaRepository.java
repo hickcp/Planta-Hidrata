f6c7158097b3028f08730c2e00cc0d926e1cab7f
rafakiyota <rfkiyota@gmail.com>
2023-04-27 23:47:03 -0300

    remocao de imports nao utilizados

--- src/main/java/com/api/rega/repository/PlantaRepository.java
+++ src/main/java/com/api/rega/repository/PlantaRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
-
 public interface PlantaRepository extends JpaRepository<Planta, Long> {
 
 
