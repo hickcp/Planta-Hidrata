c77fe192cc5e9ef8697050611692233a41f363c6
hickcp <henrique_c_p@outlook.com>
2023-05-20 10:39:45 -0300

    Remoção @transactional

--- src/main/java/com/api/rega/entity/PlantaInformacoes.java
+++ src/main/java/com/api/rega/entity/PlantaInformacoes.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="planta_informacoes")
