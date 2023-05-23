ba1f40ec681bfaef7eb29344c23a32bf1ff3d97b
hickcp <henrique_c_p@outlook.com>
2023-05-23 15:41:37 -0300

    Cors configuration
    removed unused imports

--- src/main/java/com/api/rega/repository/PlantaRepository.java
+++ src/main/java/com/api/rega/repository/PlantaRepository.java
@@ -1,11 +1,8 @@
 package com.api.rega.repository;
 
-import com.api.rega.dto.planta.DadosPlantaNome;
 import com.api.rega.entity.Planta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface PlantaRepository extends JpaRepository<Planta, Long> {
 
