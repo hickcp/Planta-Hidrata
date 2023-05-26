6060ab9ae25c075fee0e25e00f273cac0643de5b
hickcp <henrique_c_p@outlook.com>
2023-05-26 14:00:20 -0300

    new test added

--- src/test/java/com/api/rega/service/PlantaServiceTest.java
+++ src/test/java/com/api/rega/service/PlantaServiceTest.java
@@ -11,7 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
@@ -48,7 +47,6 @@ class PlantaServiceTest {
 
     @Test
     public void testNomesException(){
-
         List<Planta> vazio = new ArrayList<Planta>();
         Mockito.when(repo.findAll()).thenReturn(vazio);
 
